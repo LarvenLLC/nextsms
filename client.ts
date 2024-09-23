d7091f194ab17ea7bc34ae2abc2064bd359b3ca5
Adam Beleko <bossbele@larven.io>
2024-09-23 23:07:32 +0300

    client

--- client.ts
+++ client.ts
@@ -2,14 +2,16 @@ require('dotenv').config();
 const axios = require('axios');
 
 const environment = process.env.NEXTSMS_ENV === 'TEST' ? '/test' : '';
+const usernamePasswordBuffer = Buffer.from(process.env.NEXTSMS_USERNAME + ':' + process.env.NEXTSMS_PASSWORD);
+const base64data = usernamePasswordBuffer.toString('base64');
 
 const client = axios.create({
   baseURL: process.env.NEXTSMS_BASE_URL + environment,
   maxBodyLength: Infinity,
-  auth: {
-    username: process.env.NEXTSMS_USERNAME,
-    password: process.env.NEXTSMS_PASSWORD,
-  },
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Basic ${base64data}`,
+  }
 });
 
 export default client;
