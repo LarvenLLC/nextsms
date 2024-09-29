23c96585695369aa81b4e1dfd93904709795d361
Adam Beleko <bossbele@larven.io>
2024-09-29 10:59:19 +0300

    Export all named exports from the sms module

--- index.ts
+++ index.ts
@@ -2,4 +2,8 @@ require('dotenv').config();
 
 import * as sms from "./sms";
 
+// Export all named exports from the sms module
+export * from "./sms";
+
+// Export the default object containing all functions from the sms module
 export default sms;
